444840479f634eeb2990182dff971eb40ee3d500
Samarth Patel <samarthxpatel@gmail.com>
2021-06-13 21:59:50 -0400

    Update BottomTabBar

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -11,14 +11,49 @@ export const Navigation = () => {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen
-          name="Home"
+          name="home"
           component={Home}
           options={{
+            tabBarLabel: 'Home',
             tabBarIcon: ({color}) => <Icon name="home" color={color} />,
           }}
         />
-        <Tab.Screen name="Search" component={Home} />
-        <Tab.Screen name="Settings" component={Home} />
+        <Tab.Screen
+          name="explore"
+          component={Home}
+          options={{
+            tabBarLabel: 'Explore',
+            tabBarIcon: ({color}) => <Icon name="search" color={color} />,
+          }}
+        />
+        <Tab.Screen
+          name="trending"
+          component={Home}
+          options={{
+            tabBarLabel: 'Trending',
+            tabBarIcon: ({color}) => (
+              <Icon name="local-fire-department" color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="saved"
+          component={Home}
+          options={{
+            tabBarLabel: 'Saved',
+            tabBarIcon: ({color}) => (
+              <Icon name="bookmark-border" color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="account"
+          component={Home}
+          options={{
+            tabBarLabel: 'Account',
+            tabBarIcon: ({color}) => <Icon name="face" color={color} />,
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
